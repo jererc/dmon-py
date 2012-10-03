686b7d1443e739db660ad981af02bfaa1d4c6c18
Jerome Clerc <jererc@gmail.com>
2012-10-03 22:57:32 +0200

    clean js

--- dtools/webui/static/js/services.js
+++ dtools/webui/static/js/services.js
@@ -1,63 +1,7 @@
-function updateStatus() {
-    $('.content_element').each( function(result) {
-        var title = $(this).find('.title');
-        var img = $(this).find('.status img');
-
-        $.getJSON($SCRIPT_ROOT + '/status',
-            {name: $(this).find('input[name="name"]').val()},
-            function(data) {
-
-                if (data.result) {
-                    title.css('color', 'green');
-                    img.attr('alt', 'stop');
-                    img.attr('src', '/static/img/stop.png');
-                    }
-                else {
-                    title.css('color', '#555');
-                    img.attr('alt', 'start');
-                    img.attr('src', '/static/img/start.png');
-                    }
-
-                });
-        });
-    };
-
-$(function() {
-    updateStatus();
-    status_interval = window.setInterval(updateStatus, 2000);
-    });
-
-$(function() {
-    var interval = null;
-    var overlay_pre = null;
-    var get_log = null;
-
-    function update_log() {
-        overlay_pre.load(get_log);
-        };
-
-    $('a[rel="#log_overlay"]').overlay({
-        mask: 'black',
-        top: 'center',
-        onBeforeLoad: function() {
-            overlay_pre = this.getOverlay().find('.contentWrap');
-            get_log = this.getTrigger().attr('href');
-            overlay_pre.load(get_log);
-
-            interval = window.setInterval(update_log, 2000);
-            },
-        onClose: function() {
-            window.clearInterval(interval);
-            }
-        });
-    });
-
-$(function() {
+function initAddAction() {
     $('.button').bind('click', function() {
         var action = this.value;
-        var div = $(this).parents('.content_element')[0];
         var form = $(this).parents('form').serializeArray();
-        // Add the submit button value
         form.push({'name': 'action', 'value': action});
 
         $.getJSON($SCRIPT_ROOT + '/action',
@@ -65,22 +9,23 @@ $(function() {
             function(data) {
                 location.reload();
                 });
+
         return false;
         });
-    });
+    };
 
-$(function() {
+function initEditActions() {
     $('.img_button[alt!="log"]').bind('click', function() {
         var action = $(this).attr('alt');
         var div = $(this).parents('.content_element')[0];
-        var form = $(this).parents('form').serializeArray();
-        // Add the submit button value
-        form.push({'name': 'action', 'value': action});
 
         if (action == 'edit') {
             $(div).find('.element_edit').slideToggle();
             }
         else {
+            var form = $(this).parents('form').serializeArray();
+            form.push({'name': 'action', 'value': action});
+
             $.getJSON($SCRIPT_ROOT + '/action',
                 form,
                 function(data) {
@@ -92,6 +37,40 @@ $(function() {
                         }
                     });
             }
+
         return false;
         });
+    };
+
+function updateStatus() {
+    if (has_focus) {
+        $('.content_element').each( function(result) {
+            var title = $(this).find('.title');
+            var img = $(this).find('.status img');
+
+            $.getJSON($SCRIPT_ROOT + '/status',
+                {name: $(this).find('input[name="name"]').val()},
+                function(data) {
+
+                    if (data.result) {
+                        title.css('color', 'green');
+                        img.attr('alt', 'stop');
+                        img.attr('src', '/static/img/stop.png');
+                        }
+                    else {
+                        title.css('color', '#555');
+                        img.attr('alt', 'start');
+                        img.attr('src', '/static/img/start.png');
+                        }
+
+                    });
+            });
+        }
+    };
+
+$(function() {
+    initAddAction();
+    initEditActions();
+    updateStatus();
+    var status_interval = window.setInterval(updateStatus, 2000);
     });
